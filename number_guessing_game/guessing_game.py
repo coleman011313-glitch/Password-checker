ba60db35f068ebbd4a2e2b1cd17df715989be42f
coleman011313-glitch <coleman011313@gmail.com>
2026-06-19 17:34:13 +0100

    Clean and stabilise guessing game v2

--- number_guessing_game/guessing_game.py
+++ number_guessing_game/guessing_game.py
@@ -4,16 +4,27 @@ number = random.randint(1, 10)
 guess = 0
 attempts = 0
 
-while guess != number and attempts < 5:
-    guess = int(input("Guess a number between 1 and 10: "))
-    attempts = attempts + 1
-
+def check_guess(guess, number):
     if guess == number:
         print("Correct!")
     elif guess > number:
         print("Too high")
     else:
         print("Too low")
+
+number = random.randint(1, 10)
+guess = None
+attempts = 0 
+max_attempts = 5
+
+print("Welcome to the Guessing game!")
+
+while guess !=number and attempts < max_attempts:
+    guess = int(input("Guess a number between 1 and 10: "))
+    attempts += 1
+
+    check_guess(guess, number )
+    print("Attempts left:", max_attempts - attempts)
 
 if guess != number:
     print("Game over! The number was", number)
